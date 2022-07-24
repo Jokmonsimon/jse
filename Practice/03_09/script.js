6fe1d0201aa23dcaa6c89e5ad82fcf64420455e6
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-07-24 11:35:33 +0300

    Assignment complete

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -29,12 +29,30 @@ const backpack = {
   newVolume: function (updateVolume) {
     this.volume = updateVolume;
   },
+  newColor: function (updateColor) {
+    this.color = updateColor;
+  },
+  newpocketNum: function (updatePocketNum) {
+    this.pocketNum = updatePocketNum;
+  },
 };
 
+// Update Backpack Name
 console.log('Backpack Name Before:', backpack.name);
-backpack.newName('Back Bag');
+backpack.newName('My New Backpack');
 console.log('Backpack Name After:', backpack.name);
 
+// Update Backpack Volume
 console.log('Back Bag Volume Before:', backpack.volume);
 backpack.newVolume(55);
 console.log('Back Bag Volume After:', backpack.volume);
+
+// Update Backpack Color
+console.log('Backpack Color Before:', backpack.color);
+backpack.newColor('Black');
+console.log('Backpack Color After:', backpack.color);
+
+// Update Backpack Pocket Number
+console.log('Backpack Pocket Number Before:', backpack.pocketNum);
+backpack.newpocketNum(24);
+console.log('Backpack Pocket Number After:', backpack.pocketNum);
